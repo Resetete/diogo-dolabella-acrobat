2bf306e9f379732c5f515e8acf383a8d84715d3d
Theresa <tmannschatz@gmail.com>
2020-01-16 23:19:11 +0100

    add video slider

--- js/main.js
+++ js/main.js
@@ -38,62 +38,42 @@ $(document).ready(function() {
     }, 500)
   })
 
-// video section
+// video section slider
 
-$.global = new Object();
+var currentIndex = 0,
+  items = $('.container div'),
+  itemAmt = items.length;
 
-$.global.item = 1;
-$.global.total = 0;
+function cycleItems() {
+  var item = $('.container div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
 
-$(document).ready(function()
- {
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
 
- var WindowWidth = $(window).width();
- var SlideCount = $('#slides li').length;
- var SlidesWidth = SlideCount * WindowWidth;
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
 
-  $.global.item = 0;
-   $.global.total = SlideCount;
-
- $('.slide').css('width',WindowWidth+'px');
- $('#slides').css('width',SlidesWidth+'px');
-
-  $("#slides li:nth-child(1)").addClass('alive');
-
- $('#left').click(function() { Slide('back'); });
- $('#right').click(function() { Slide('forward'); });
-
- });
-
-function Slide(direction)
- {
-
-   if (direction == 'back') { var $target = $.global.item - 1; }
-   if (direction == 'forward') { var $target = $.global.item + 1; }
-
-   if ($target == -1) { DoIt($.global.total-1); }
-   else if ($target == $.global.total) { DoIt(0); }
-   else { DoIt($target); }
-
-
- }
-
-function DoIt(target)
- {
-
-   var $windowwidth = $(window).width();
- var $margin = $windowwidth * target;
-   var $actualtarget = target+1;
-
-   $("#slides li:nth-child("+$actualtarget+")").addClass('alive');
-
-   $('#slides').css('transform','translate3d(-'+$margin+'px,0px,0px)');
-
-   $.global.item = target;
-
- $('#count').html($.global.item+1);
-
- }
-
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+  });
 
 });
