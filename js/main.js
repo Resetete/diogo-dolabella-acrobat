abb8291986499784396bce15ab8d606a51f23f6f
Theresa <tmannschatz@gmail.com>
2020-08-19 11:25:36 +0200

    Add testemonials, add testemonial slider, calculate gallery background height based on vh

--- js/main.js
+++ js/main.js
@@ -1,41 +1,86 @@
 $(document).ready(function() {
 
-// video section slider
+  // video section slider
 
-var currentIndex = 0,
+  var currentIndex = 0,
   items = $('.container div'),
   itemAmt = items.length;
 
-function cycleItems() {
-  var item = $('.container div').eq(currentIndex);
-  items.hide();
-  item.css('display','inline-block');
-}
-
-var autoSlide = setInterval(function() {
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
+  function cycleItems() {
+    var item = $('.container div').eq(currentIndex);
+    items.hide();
+    item.css('display','inline-block');
   }
-  cycleItems();
-}, 3000);
 
-$('.next').click(function() {
-  clearInterval(autoSlide);
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
+  var autoSlide = setInterval(function() {
+    currentIndex += 1;
+    if (currentIndex > itemAmt - 1) {
+      currentIndex = 0;
+    }
+    cycleItems();
+  }, 3000);
+
+  $('.next').click(function() {
+    clearInterval(autoSlide);
+    currentIndex += 1;
+    if (currentIndex > itemAmt - 1) {
+      currentIndex = 0;
+    }
+    cycleItems();
+  });
+
+  $('.prev').click(function() {
+    clearInterval(autoSlide);
+    currentIndex -= 1;
+    if (currentIndex < 0) {
+      currentIndex = itemAmt - 1;
+    }
+    cycleItems();
+  });
+
+
+
+
 });
 
-$('.prev').click(function() {
-  clearInterval(autoSlide);
-  currentIndex -= 1;
-  if (currentIndex < 0) {
-    currentIndex = itemAmt - 1;
+$(document).ready(function() {
+
+  // testemonial section slider
+
+  var currentIndexTestemonial = 0,
+  itemsTestemonial = $('.container-testemonial div'),
+  itemAmtTestemonial = itemsTestemonial.length;
+
+  function cycleItems() {
+    var itemTestemonial = $('.container-testemonial div').eq(currentIndexTestemonial);
+    itemsTestemonial.hide();
+    itemTestemonial.css('display','inline-block');
   }
-  cycleItems();
+
+  var autoSlideTestemonial = setInterval(function() {
+    currentIndexTestemonial += 1;
+    if (currentIndexTestemonial > itemAmtTestemonial - 1) {
+      currentIndexTestemonial = 0;
+    }
+    cycleItems();
+  }, 5000);
+
+  $('.next-testemonial').click(function() {
+    clearInterval(autoSlideTestemonial);
+    currentIndexTestemonial += 1;
+    if (currentIndexTestemonial > itemAmtTestemonial - 1) {
+      currentIndexTestemonial = 0;
+    }
+    cycleItems();
+  });
+
+  $('.prev-testemonial').click(function() {
+    clearInterval(autoSlideTestemonial);
+    currentIndexTestemonial -= 1;
+    if (currentIndexTestemonial < 0) {
+      currentIndexTestemonial = itemAmtTestemonial - 1;
+    }
+    cycleItems();
   });
 
 });
