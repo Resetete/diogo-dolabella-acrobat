24112a0f12ad8663255958ba1a783294ce78acd6
Theresa <tmannschatz@gmail.com>
2020-08-19 16:09:24 +0200

    Remove padding and margin below video header

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ $(document).ready(function() {
 
     $(this)
     .data('aspectRatio', this.height / this.width)
-
     // and remove the hard coded width/height
     .removeAttr('height')
     .removeAttr('width');
@@ -65,12 +64,10 @@ $(document).ready(function() {
 
     // Resize all videos according to their own aspect ratio
     $allVideos.each(function() {
-
       var $el = $(this);
       $el
       .width(newWidth*0.6)
       .height(newWidth * $el.data('aspectRatio'));
-
     });
 
     // Kick off one resize to fix all videos on page load
